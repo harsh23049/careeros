99f4dcf9a234676de4db4107264aa42770847190
harsh23049 <cse23049@iiitkalyani.ac.in>
2026-08-07 15:31:50 +0530

    Refactor environment configuration: Update .env.sample and remove deprecated env.js; add MongoDB connection setup

---
+++ apps/backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connnectDB = async () => {
+  try{
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    console.log(`Database: ${connectionInstance.connection.name}`);
+
+  }
+  catch(error){
+    console.error("Error connecting to MongoDB:", error);
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+export default connnectDB;
