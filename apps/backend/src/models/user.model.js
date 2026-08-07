81c0786a4487657e1f24d2d34d9e38f2e1f57ed8
harsh23049 <cse23049@iiitkalyani.ac.in>
2026-08-08 01:51:41 +0530

    Refactor user model: Update password handling with bcrypt, add JWT methods, and improve validation

--- apps/backend/src/models/user.model.js
+++ apps/backend/src/models/user.model.js
@@ -1,44 +1,112 @@
 import mongoose from "mongoose";
-import bycrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
     {
         fullName: {
-            type: String,   
+            type: String,
             required: [true, "Please provide your full name"],
             trim: true,
-            minlength: [3, "Name must be at least 3 charaters long"],
-            maxlength: [50, "Name must not be more than 50 charaters long"],
+            minlength: [3, "Name must be at least 3 characters long"],
+            maxlength: [50, "Name must not be more than 50 characters long"],
         },
-        email:{
+        username: {
+            type: String,
+            required: [true, "Please provide your username"],
+            unique: true,
+            trim: true,
+            minlength: [3, "Username must be at least 3 characters long"],
+            maxlength: [30, "Username must not be more than 30 characters long"],
+        },  
+        email: {
             type: String,
             required: [true, "Please provide your email"],
             unique: true,
+            lowercase: true,
+            trim: true,
             match: [
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}(\.[0-9]{1,3}){3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                 "Please provide a valid email",
             ],
         },
-        password:{
+
+        password: {
             type: String,
             required: [true, "Please provide a password"],
-            minlength: [6, "Password must be at least 6 charaters long"],
+            minlength: [6, "Password must be at least 6 characters long"],
         },
+
         avatar: {
-            type: String,
-            default: "https://res.cloudinary.com/dxjzq6v0g/image/upload/v1686940915/avatars/default-avatar_ow1k3r.png",
+            url: {
+                type: String,
+                default:
+                    "https://res.cloudinary.com/dxjzq6v0g/image/upload/v1686940915/avatars/default-avatar_ow1k3r.png",
+            },
+            publicId: {
+                type: String,
+                default: "",
+            },
         },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            default: "user",
-        },
+
         refreshToken: {
             type: String,
-            default: null,
+            default: "",
         },
     },
-    { timestamps: true
+    {
+        timestamps: true,
     }
-)
+);
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+    return await bcrypt.compare(password, this.password);
+
+};
+
+userSchema.methods.generateAccessToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullName: this.fullName,
+            email: this.email,
+        },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+
+};
+
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
